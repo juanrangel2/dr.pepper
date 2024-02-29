cf3e2c18c9648fa89f65f03c786802f82f203a14
juanrangel2 <143430673+juanrangel2@users.noreply.github.com>
2024-02-29 17:20:08 +0000

    first commit

---
+++ dr.js
@@ -0,0 +1,28 @@
+
+console.log("hello");
+
+
+let button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+    let age = document.querySelector(".age").value;
+    let food = document.querySelector(".food").value;
+
+
+console.log(age);
+
+
+
+
+    if (age <= 18 && food === "pizza"){
+        document.querySelector("p").innerHTML="You are original Dr.Pepper";
+        flav.style.display = "none";
+    }
+
+
+
+
+
+
+
+    });
